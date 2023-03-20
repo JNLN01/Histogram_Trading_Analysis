b5efcf97342730406b38da38cf1582079e011173
JNLN01 <j4cob_@hotmail.co.uk>
2023-03-20 11:16:10 +0000

    Initial commit

---
+++ trading_project.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Dataset
+eurusd = [66,66,100,71,71,50,100,40,33,66,100,0,100,50,83,66,50,50]
+audusd = [58,100,60,80,60,50,50,66,50,20,0,50,100,66,83,100,100,75]
+
+#Histogram
+plt.hist(eurusd, bins=10, edgecolor='black')
+plt.title("Distribution of EURUSD Weekly Rate of Reactions From S/D Zones")
+plt.xlabel('Rate of Zone Reaction (%)')
+plt.ylabel('Frequency')
+
+plt.show()
+
+plt.hist(audusd, bins=10, edgecolor='black')
+plt.title("Distribution of AUDUSD Weekly Rate of Reactions From S/D Zones")
+plt.xlabel('Rate of Zone Reaction (%)')
+plt.ylabel('Frequency')
+
+plt.show()
