f80d90974247a6c6d37b3c9166eaf6b3dbcefd02
JNLN01 <j4cob_@hotmail.co.uk>
2023-04-11 11:02:08 +0100

    Changed some formatting Added some comments

--- trading_project.py
+++ trading_project.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Dataset
+#Datasets
 eurusd = [66,66,100,71,71,50,100,40,33,66,100,0,100,50,83,66,50,50]
 audusd = [58,100,60,80,60,50,50,66,50,20,0,50,100,66,83,100,100,75]
 
 #Histogram
+#bins = splits the X axis into tenths
 plt.hist(eurusd, bins=10, edgecolor='black')
 plt.title("Distribution of EURUSD Weekly Rate of Reactions From S/D Zones")
 plt.xlabel('Rate of Zone Reaction (%)')
